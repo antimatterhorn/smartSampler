Fixed definition of h to have proper units

interpolate.py
@@ -7,14 +7,15 @@ def interp(x,y,points):
     numPoints = len(points)
     xout = np.zeros(len(points[0].state))
     for i in range(numPoints):
-        thisState = points[i].state
-        thisPos = points[i].position
-        thisCor = points[i].correction
+        thisPoint   = points[i]
+        thisState   = thisPoint.state
+        thisPos     = thisPoint.position
+        thisCor     = thisPoint.correction
+        thisWeight  = thisPoint.weight
         xx  = thisPos.x - x
         yy  = thisPos.y - y
-        rij = vector2(xx,yy)
-        rij = rij.magnitude()
-        hi  = 200.*points[i].radius**-2
+        rij = vector2(xx,yy).magnitude()
+        hi  = points[i].radius*0.5
         for j in range(len(thisState)):
-            xout[j] += thisState[j]*kernel.W(rij/hi,hi)*points[i].weight*thisCor
+            xout[j] += kernel.W(rij/hi,hi)*thisWeight*thisCor*thisState[j]
     return xout

kernel.py
@@ -1,15 +1,15 @@
 import numpy as np
 from math import *
 
-def W(r,h):
-	r = abs(r) #protection
+def W(e,h):
+	e = abs(e) #protection
 	a = 10.0/(7.0*np.pi*h**2)
-	if r>= 2.0:
+	if e>= 2.0:
 		return 0
-	elif r>=1.0:
-		return a*0.25*(2.0-r)**3
+	elif e>=1.0:
+		return a*0.25*(2.0-e)**3
 	else:
-		return a*(1.0-1.5*r**2+0.75*r**3)
+		return a*(1.0-1.5*e**2+0.75*e**3)
 
-def gradW(r,h):
+def gradW(e,h):
 	return 0.

robustSample.py
@@ -16,7 +16,7 @@ class point:
         self.neighbors = []
         self.weight = 0.0
         self.radius = 0.0
-        self.correction = 0.0
+        self.correction = 1.0
     def updateNeighbors(self,points,nCount):
         n = len(points)
         myPos = self.position
@@ -33,7 +33,7 @@ class point:
         for i in range(nCount):
             self.neighbors.append(others[i][1])
         self.radius = others[nCount-1][0]
-        self.weight = self.radius**2
+        self.weight = np.pi*self.radius**2
     def calcCorrection(self,points):
         n = len(points)
         myId = self.id
@@ -44,7 +44,7 @@ class point:
             weights += thisWeight
         self.correction = 1.0/weights
 
-numPoints = 400
+numPoints = 200
 nNeighbor = 20           
 numInputs = 2
 points = []
@@ -77,20 +77,43 @@ for i in range(numPoints):
     thisPoint = points[i]
     thisPoint.calcCorrection(points)
 
-sSize = 50
-iMap = np.zeros([sSize,sSize])
-xx = np.linspace(xmin,xmax,sSize)
-yy = np.linspace(ymin,ymax,sSize)
 
-for i in range(sSize):
-    for j in range(sSize):
-        x = (xmax-xmin)*j/sSize+xmin
-        y = (ymax-ymin)*i/sSize+ymin
-        val = interpolate.interp(x,y,points)
-        iMap[i][j] = val[0]
-        print(x,y,val)
 
-plt.plot(xs,ys,"g+")
-plt.pcolor(xx,yy,iMap)
-plt.colorbar()
+makeMap = True
+if makeMap:
+    sSize = 50
+    iMap = np.zeros([sSize,sSize])
+    xx = np.linspace(xmin,xmax,sSize)
+    yy = np.linspace(ymin,ymax,sSize)
+
+    for i in range(sSize):
+        for j in range(sSize):
+            x = (xmax-xmin)*j/sSize+xmin
+            y = (ymax-ymin)*i/sSize+ymin
+            val = interpolate.interp(x,y,points)
+            iMap[i][j] = val[1]
+            #print(x,y,val)
+    plt.pcolor(xx,yy,iMap)
+    plt.colorbar()
+
+sampleNew = False
+if sampleNew:
+    numNewSamples = 10
+    numPoints = len(points)
+    newPoints = []
+    xs = np.zeros(numNewSamples)
+    ys = np.zeros(numNewSamples)
+    for i in range(numNewSamples):
+        xn = random.random()*2+6.0
+        yn = random.random()*2+6.0
+        sn  = interpolate.interp(x,y,points)   
+        x,y = calculation.func(sn)
+        xs[i] = x
+        ys[i] = y
+        newPoints.append(point(i+numPoints,vector2(x,y),sn))
+        print(xn,yn,sn,x,y)
+    plt.plot(xs,ys,"ro")
+plt.scatter(xs,ys,c='black',marker='+')
+plt.xlim(xmin,xmax)
+plt.ylim(ymin,ymax)
 plt.show()